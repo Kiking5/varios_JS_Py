105635fc8c8bff1c885e8d38bedc3a52ed87e956
Johnattan Peña <167146327+Kiking5@users.noreply.github.com>
2024-12-29 11:34:32 -0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,15 @@
+// Ambito global
+
+var nombre = "Juan"
+console .log(nombre) // ya no es utilizado por que puede generar varios problemas
+
+
+// Ambito de bloque
+
+let apellido = "Perez"  // se utiliza mas que var ya que solo se utiliza en el bloque donde es crado
+console .log(apellido)
+
+// Constantes
+
+const PI = 3.1416 // no se puede cambiar el valor de la constante
+console.log(PI)
