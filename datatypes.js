f5a6db2bbb4a0a16442105caca6f876a3574c573
Johnattan Peña <167146327+Kiking5@users.noreply.github.com>
2024-12-29 11:35:56 -0500

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,52 @@
+// String
+
+let nombre = "John peña"
+console.log(nombre)
+console.log(typeof nombre)
+
+// Number
+
+let edad = 40 // Entero
+let estatura = 1.70 // Decimal
+console.log(edad)
+console.log(typeof edad)
+console.log(estatura)
+console.log(typeof estatura)
+
+// Boolean
+
+let esSoltero = true
+let esCasado = false
+console.log(esSoltero)
+console.log(typeof esSoltero)
+console.log(esCasado)
+console.log(typeof esCasado)
+
+
+// Undefined
+
+let apellido // Undefine
+console.log(apellido)
+console.log(typeof apellido)
+
+// Null
+
+let avion = null
+console.log(avion)
+console.log(typeof avion)
+
+//Symbol
+
+let simbolo = Symbol("mi simbolo")
+console.log(simbolo)
+console.log(typeof simbolo)
+
+// BigInt
+
+let numeroGrande = 9007199254740991654318734132154897543321343343n
+console.log(numeroGrande)
+console.log(typeof numeroGrande)
+
+let numeroGrande2 = BigInt(654678545412491654318734132154897543321343343)
+console.log(numeroGrande2)
+console.log(typeof numeroGrande2)
