b3469ad62e518661d0405f138d8b04f9c6d840f0
Johnattan Peña <167146327+Kiking5@users.noreply.github.com>
2025-01-27 13:50:59 -0500

    Add files via upload

---
+++ ejerciciosBasicos.js
@@ -0,0 +1,86 @@
+// 1. Escribe un comentario en una línea
+
+//Hola, soy un comentario en una línea
+
+// 2. Escribe un comentario en varias líneas
+
+/*
+Hola, soy un comentario
+en varias líneas
+*/
+
+// 3. Declara variables con valores asociados a todos los datos de tipo 
+// primitivos
+
+let numero = 10
+let cadena = "Hola"
+let booleano = true
+let indefinido = undefined
+let nulo = null
+let simbolo = Symbol("simbolo")
+let BigInt = 1005646545643216131345678549874n
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log(numero)
+console.log(cadena)
+console.log(booleano)
+console.log(indefinido)
+console.log(nulo)
+console.log(simbolo)
+console.log(BigInt)
+
+// 5. Imprime por consola el tipo de todas las variables
+
+console.log(typeof numero)
+console.log(typeof cadena)
+console.log(typeof booleano)
+console.log(typeof indefinido)
+console.log(typeof nulo)
+console.log(typeof simbolo)
+console.log(typeof BigInt)
+
+// 6. A continuación, modifica los valores de las variables por otros del 
+// mismo tipo
+
+numero = 20
+cadena = "Adios"
+booleano = false
+indefinido = null
+nulo = undefined
+simbolo = Symbol("otro simbolo")
+BigInt = 54132132163543143421342134221n
+
+// 7. A continuación, modifica los valores de las variables por otros de 
+// distinto tipo
+
+numero = "Hola"
+cadena = 10
+booleano = 100
+indefinido = true
+nulo = "Adios"
+simbolo = 100
+BigInt = "Hola"
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos 
+// primitivos
+
+const numero2 = 10
+const cadena2 = "Hola"
+const booleano2 = true
+const indefinido2 = undefined
+const nulo2 = null
+const simbolo2 = Symbol("simbolo")
+const BigInt2 = 6754675454645413213213213131344n
+
+// 9. A continuación, modifica los valores de las constantes
+
+// numero2 = 20
+// cadena2 = "Adios"
+// booleano2 = false
+// indefinido2 = null
+// nulo2 = undefined
+// simbolo2 = Symbol("otro simbolo")
+// BigInt2 = 54132132163543143421342134221n
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
