ff5e0682a23f4a9c01f068917afd7d4d78761575
Johnattan Peña <167146327+Kiking5@users.noreply.github.com>
2025-03-18 19:11:44 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,33 @@
+""" //Ambito global
+
+var nombre = "Juan"
+console .log(nombre) // ya no es utilizado por que puede generar varios problemas
+
+
+// Ambito de bloque
+
+let apellido = "Perez"  // se utiliza mas que var ya que solo se utiliza en el bloque donde es crado
+console .log(apellido)
+
+// Constantes
+
+const PI = 3.1416 // no se puede cambiar el valor de la constante
+console.log(PI) """
+
+nombre = "Johnattan"
+print(nombre)
+print(type(nombre))
+
+edad = 40 # no se utiliza let ni var para la creación de variables, python es de tipado dinámico
+print(edad)
+print(type(edad))
+
+es_de_dia = True
+print(es_de_dia)
+print(type(es_de_dia))
+
+peso = 65.5
+print(peso)
+print(type(peso))
+
+# No se pueden crear constantes en python
