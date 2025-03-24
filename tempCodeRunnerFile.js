67afbc1ce8f8ab65b8ea9f23c1dc5f10c969a06e
Johnattan Peña <167146327+Kiking5@users.noreply.github.com>
2025-03-24 11:55:11 -0500

    Add files via upload

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,11 +1,2 @@
-import despedir, { saludo, myName, Person } from "./modules-exercises";
-
-console.log(saludo())
-console.log(myName)
-
-const persona = new Person("Johnattan", 30, "Kiking5")
-console.log(persona.name)
-console.log(persona.age)
-console.log(persona.alias)
-
-console.log(despedir())
+let sum = numbers.reduce((result, current) => result + current)
+console.log(sum)
