aa05d13ff655c6cf274c6a540381c44e0ef3a528
Johnattan Peña <167146327+Kiking5@users.noreply.github.com>
2025-03-27 12:13:03 -0500

    Add files via upload

---
+++ calcular_distancia.py
@@ -0,0 +1,13 @@
+""" Se desea calcular la distancia recirrida (m) por un móvil que tiene una velocidad constante (m/s) durante un tiempo T (Sg), considerar que es un movimiento rectilineo uniforme """
+
+""" 
+Distancia recorrida (m) = D
+Velocidad constante (m/s) = V
+Tiempo (sg) = T
+"""
+
+v = int(input("Ingrese la velocidad (m/s): "))
+t = int(input("Ingrese el tiempo (sg): "))
+d = v * t
+
+print("La distancia recorrida es: ", d, "m")
