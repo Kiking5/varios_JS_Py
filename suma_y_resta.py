db7f6747935876736ac378ade148c4796f9217ea
Johnattan Peña <167146327+Kiking5@users.noreply.github.com>
2025-04-09 12:46:41 -0500

    Add files via upload

---
+++ suma_y_resta.py
@@ -0,0 +1,10 @@
+""" Escribe un programa que pida dos números al usuario y muestre la suma y la resta de ambos. """
+
+a = int(input("Ingrese un número: "))
+b = int(input("Ingrese otro número: "))
+
+suma = (a + b)
+resta = (a - b)
+
+print(f"la suma de {a} y {b} es {suma}")
+print(f"la resta de {a} y {b} es {resta}")
