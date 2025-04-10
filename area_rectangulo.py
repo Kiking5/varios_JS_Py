b23cf52ca2df55fdc3a8aa7b4a8c076b6eac76ce
Johnattan Peña <167146327+Kiking5@users.noreply.github.com>
2025-04-10 11:36:48 -0500

    Add files via upload

---
+++ area_rectangulo.py
@@ -0,0 +1,7 @@
+""" Pide al usuario la base y la altura de un rectángulo y muestra su área. """
+
+base = int(input("Ingrese la base del rectángulo: "))
+altura = int(input("Ingrese la altura del rectángulo: "))
+
+area = base * altura
+print(f"El área del rectángulo es {area}")
