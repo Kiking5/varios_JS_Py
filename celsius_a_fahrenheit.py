0983e603a1c1fdaa8b7547d03a7fda8291a77dfb
Johnattan Peña <167146327+Kiking5@users.noreply.github.com>
2025-04-12 10:19:57 -0500

    Add files via upload

---
+++ celsius_a_fahrenheit.py
@@ -0,0 +1,5 @@
+""" Convierte una temperatura de grados Celsius a Fahrenheit. Usa la fórmula: """
+
+celsius = float(input("Introduce la temperatura en grados Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} grados Celsius son {fahrenheit} grados Fahrenheit.")
